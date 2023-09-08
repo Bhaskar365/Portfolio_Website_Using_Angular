32f9ea9de62e06e813752296b36b608627b6c304
bdas615 <bdas@brandinstitute.com>
2023-09-08 19:54:57 +0530

    breakpointObserver cdk directive for responsive phone and pc screens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BannerComponent } from './banner/banner.component';
 import { PersonalInformationComponent } from './personal-information/personal-information.component';
 import { EducationComponent } from './education/education.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { BannerComponent } from './banner/banner.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ResponsiveDirective } from './directives/responsive.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BannerComponent,
     PersonalInformationComponent,
     EducationComponent,
     SkillsComponent,
     ProjectsComponent,
     ContactComponent,
-    WorkExperienceComponent
+    WorkExperienceComponent,
+    BannerComponent,
+    ResponsiveDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css']
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit {
+
+  // @HostBinding("class.pc") pcMode = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit(): void {
+      // this.breakpointObserver.observe([Breakpoints.HandsetPortrait,Breakpoints.WebLandscape])
+      // .subscribe({
+      //   next:(result:any)=>{
+      //     for(let breakpoint of Object.keys(result.breakpoints)) {
+      //       if(result.breakpoints[breakpoint]){
+      //         if(breakpoint == Breakpoints.HandsetPortrait) {
+      //             this.pcMode = false;
+      //         }
+      //         if(breakpoint == Breakpoints.WebLandscape) {
+      //             this.pcMode = true;
+      //         }
+
+      //       }
+      //     }
+      //   }
+      // })
+}
 
 }
+ 
