6c3e88bb6f1db494ea84ee9dd6b5c2b5971b6232
bdas615 <bdas@brandinstitute.com>
2023-09-08 21:12:56 +0530

    implementing banner component

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -8,28 +8,9 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 })
 export class BannerComponent implements OnInit {
 
-  // @HostBinding("class.pc") pcMode = false;
+  constructor() { }
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
-
-  ngOnInit(): void {
-      // this.breakpointObserver.observe([Breakpoints.HandsetPortrait,Breakpoints.WebLandscape])
-      // .subscribe({
-      //   next:(result:any)=>{
-      //     for(let breakpoint of Object.keys(result.breakpoints)) {
-      //       if(result.breakpoints[breakpoint]){
-      //         if(breakpoint == Breakpoints.HandsetPortrait) {
-      //             this.pcMode = false;
-      //         }
-      //         if(breakpoint == Breakpoints.WebLandscape) {
-      //             this.pcMode = true;
-      //         }
-
-      //       }
-      //     }
-      //   }
-      // })
-}
+  ngOnInit(): void { }
 
 }
  
