ab48db07e50de2c8dc146ec3babf231e7c52a16e
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2023-12-28 18:26:23 +0530

    test commit to check github calendar issue

--- src/app/personal-information/personal-information.component.ts
+++ src/app/personal-information/personal-information.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-personal-information',
   templateUrl: './personal-information.component.html',
   styleUrls: ['./personal-information.component.css']
 })
-export class PersonalInformationComponent {
+export class PersonalInformationComponent implements OnInit{
+
+  myData : string [][] = [
+    ['Name', 'Sundar Pichai'],
+    ['DOB', '10/06/1972'],
+    ['Work Exp', '25 Years'],
+    ['Education', 'M.S. (1995)'],
+    ['Interests', 'Cricket']
+  ];
+
+//test text
+
+  aboutMe:string[] = [
+    'Hi sofwejlkjwelkfjwelkfj wlkejflkwejflkwejfkl;wjef;lkwjef;lkjweflkjwe;lfjwe;if jwlkejflkwejflkjwefj ',
+    'asdkfj hldfhelk ejlkf elkfj elkf jelf ej'
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
 
 }
+
