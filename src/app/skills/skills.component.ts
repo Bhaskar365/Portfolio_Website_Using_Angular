165d0b73e30df6718b3b7511483d19c585090f54
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-01-11 14:35:35 +0530

    skills component

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -9,7 +9,31 @@ import { Skill } from '../models/models';
 export class SkillsComponent implements OnInit{
 
   skills: Skill[] = [
-    
+    {
+      name:'Angular, Angular Material',
+      level: 'Expert',
+      rating:95
+    },
+    {
+      name: 'ASP.NET Core, EF Core',
+      level: 'Intermediate',
+      rating : 75
+    },
+    {
+      name: 'HTML, CSS, JS',
+      level: 'Expert',
+      rating: 90
+    },
+    {
+      name : 'Java',
+      level: 'Expert',
+      rating: 90
+    },
+    {
+      name:'C++',
+      level:'Intermediate',
+      rating:80
+    }
   ];
 
   ngOnInit(): void {
