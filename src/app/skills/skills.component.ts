432574893120cbb4b9412edddd3df4b4220e43b8
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-01-11 13:25:11 +0530

    work exp page

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Skill } from '../models/models';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css']
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit{
+
+  skills: Skill[] = [
+    
+  ];
+
+  ngOnInit(): void {
+    
+  }
 
 }
--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -10,6 +10,42 @@ export class WorkExperienceComponent implements OnInit{
 
   workExpList:WorkExperience[] = [
     {
+      role: "CEO",
+      company: "McKinsey & Co.",
+      duration: "2019-Present",
+      description: [
+        'Worked on different technologies such as .NET , JAVA, C++',
+        'Worked with multiple teams to develop desktop and desktop applications'
+      ]
+    },
+    {
+      role: "Senior Vice President",
+      company: "McKinsey & Co.",
+      duration: "2012-2019",
+      description: [
+        'Worked on different technologies such as .NET , JAVA, C++',
+        'Worked with multiple teams to develop desktop and desktop applications'
+      ]
+    },
+    {
+      role: "Vice President",
+      company: "McKinsey & Co.",
+      duration: "2008-2012",
+      description: [
+        'Worked on different technologies such as .NET , JAVA, C++',
+        'Worked with multiple teams to develop desktop and desktop applications'
+      ]
+    },
+    {
+      role: "Product Manager",
+      company: "McKinsey & Co.",
+      duration: "2005-2008",
+      description: [
+        'Worked on different technologies such as .NET , JAVA, C++',
+        'Worked with multiple teams to develop desktop and desktop applications'
+      ]
+    },
+    {
       role: "Software Developer",
       company: "McKinsey & Co.",
       duration: "2003-2005",
